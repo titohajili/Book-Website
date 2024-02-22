777377d1cbf21ca81824208ec68765c16d9e782f
titohajili <maqatoto963@gmail.com>
2024-02-22 13:40:15 +0400

    Section Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,25 @@ if(loginClose){
         loginContent.classList.remove('show-login')
     })
 };
+
+// swiper home
+
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView: 'auto', 
+    centeredSlides: "auto",
+
+    autoplay: {
+        delay:3000,
+        disableOnInteraction: false,
+
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -32,
+        }
+    }
+})
