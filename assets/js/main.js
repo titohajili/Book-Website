001696042045209f3799924b352be67017ed1c3f
titohajili <maqatoto963@gmail.com>
2024-02-22 10:33:30 +0400

    Login

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,4 +13,22 @@ if(saearchClose){
     saearchClose.addEventListener('click', () =>{
         searchContent.classList.remove('show-search')
     })
+};
+
+//===== LOGIN =======
+
+const loginButton = document.getElementById('login-button')
+const loginClose = document.getElementById('login-close')
+const loginContent = document.getElementById('login-content')
+
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
 }
+
+if(loginClose){
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
+};
