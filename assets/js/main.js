2aa4189edd6afcaa408e109b3cdaed7730f498bd
titohajili <maqatoto963@gmail.com>
2024-02-26 18:18:06 +0400

    Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,3 +122,35 @@ let swiperTestimonial = new Swiper('.testimonial__swiper', {
         }
     }
   });
+
+  //SCROLL UP
+
+  const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                : scrollUp.classList.remove('show-scroll')
+  }
+
+  window.addEventListener('scroll', scrollUp)
+ 
+  const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
